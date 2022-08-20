b8f98b69089e7e5e5f024d5febae221825ba7d43
Inderjit57 <singhinderjit57@yahoo.com>
2022-08-20 12:01:11 -0400

    Added verification step to verify polling count page

--- src/test/java/com/Slashdot/TakeHomeTest/SlashdotTest.java
+++ src/test/java/com/Slashdot/TakeHomeTest/SlashdotTest.java
@@ -1,5 +1,7 @@
 package com.Slashdot.TakeHomeTest;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -12,12 +14,14 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SlashdotTest {
 	WebDriver wd;
 	WebDriverWait wait;
+	SoftAssert sf = new SoftAssert();
 
 	@BeforeMethod
 	public void setupDriver() {
@@ -31,8 +35,11 @@ public class SlashdotTest {
 		wd = new ChromeDriver();
 
 		wd.get("https://slashdot.org/");
+
+		// Explicit wait to wait until the expected condition is true
 		wait = new WebDriverWait(wd, 10);
 
+		// Maximizing the window once opened
 		wd.manage().window().maximize();
 	}
 
@@ -50,31 +57,40 @@ public class SlashdotTest {
 
 		for (WebElement elements : uniqueIcons) {
 			System.out.println("Icon present: " + elements.getAttribute("title"));
+
+//			System.out.println(Collections.frequency(uniqueIcons, elements));
 		}
 
 		// 4. Vote for some random option on the daily poll
 
-		// Conditionally waiting for the element to be clickable: voting window
+		/* Conditionally waiting for the element to be clickable: voting window */
 		wait.until(ExpectedConditions
 				.elementToBeClickable(By.cssSelector("form[id='pollBooth'] :nth-of-type(4) input[type='radio']")));
-		WebElement votingPoll = wd
-				.findElement(By.cssSelector("form[id='pollBooth'] :nth-of-type(4) input[type='radio']"));
-		votingPoll.click();
+		wd.findElement(By.cssSelector("form[id='pollBooth'] :nth-of-type(4) input[type='radio']")).click();
 		wd.findElement(By.cssSelector("form[id='pollBooth']  div button")).click();
 
-		System.out.println(wd
+		System.out.println("Selected poll item: " + wd
 				.findElement(By.cssSelector(
 						"div[class='row split-container'] div div:nth-of-type(4) >div[class='poll-bar-label']"))
 				.getText());
 
 		// 5. Return the number of people that have voted for that same option
 
-		// Conditionally waiting for the element to be visible
+		/* Conditionally waiting for the element to be visible */
 		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector(
 				"div[class='row split-container'] div div:nth-of-type(4) >div[class='poll-bar-bg'] div div")));
 		WebElement totalVotesOnPoll = wd.findElement(By.cssSelector(
 				"div[class='row split-container'] div div:nth-of-type(4) >div[class='poll-bar-bg'] div div"));
+
+		/*
+		 * Verifying if page is opened using soft assetion
+		 */
+		String votePollingPage = wd.getTitle();
+		System.out.println("current page title: " + votePollingPage);
+		sf.assertEquals(votePollingPage, "Slashdot Poll | What is your favorite BSD operating ...");
+
 		System.out.println("total votes on poll: " + totalVotesOnPoll.getText());
+		sf.assertAll();
 	}
 
 	@AfterMethod
